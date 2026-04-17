a2523af7af08d7c9566273bf9fd88d1ff83c81aa
niilopal <niilopal@metropolia.fi>
2026-04-17 10:17:20 +0300

    Module 4 Commit. Fixed CORS error by implementing API using flask.

---
+++ Module4/CORS.py
@@ -0,0 +1,14 @@
+#Must be running for all exercises of the module except ex 1
+from flask import Flask
+from flask_cors import CORS
+import requests, json
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS-Headers'] = 'Content-Type'
+
+@app.route('/shows/<term>')
+def shows(term):
+    dict = requests.get(f'https://api.tvmaze.com/search/shows?q={term}')
+    return dict.json()
+app.run(use_reloader = True, host = '127.0.0.1', port = 3000)
--- Module4/ex4.js
+++ Module4/ex4.js
@@ -1,6 +1,7 @@
+//Requires running CORS.py api
 document.querySelector("form").addEventListener('submit', async function(submit) {
     submit.preventDefault()
-    const url = `https://api.tvmaze.com/search/shows&q${document.querySelector("input[name='q']").value}`
+    const url = `http://127.0.0.1:3000/shows/${document.querySelector("input[name='q']").value}`
     try {
         const response = await fetch(url)
         const json = await response.json()
